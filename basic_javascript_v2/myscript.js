4a1fdcdfe457a20f607da7005e00f99909c7d688
tsubakimoto_s <tkmbmbr@gmail.com>
2014-06-15 22:55:45 +0900

    #10-#11

--- basic_javascript_v2/myscript.js
+++ basic_javascript_v2/myscript.js
@@ -24,9 +24,28 @@
 //        alert("wrong signal");
 //        break;
 //}
-var i = 0;
-while (i < 10) {
-    console.log(i);
-    i++;
+//var i = 0;
+//while (i < 10) {
+//    console.log(i);
+//    i++;
+//}
+//for (var i = 0; i < 5; i++) {
+//    if (i == 1) {
+//        continue;
+//    }
+//    alert(i);
+//    if (i == 3) {
+//        break;
+//    }
+//}
+//var answer = confirm("sure?");
+//if (answer) {
+//    alert("ok");
+//} else {
+//    alert("ng");
+//}
+var name = prompt("お名前は？", "名無しさん");
+if (name != null) {
+    alert(name);
 }
 //# sourceMappingURL=myscript.js.map
