e64d28763d69c1c036dd01bf54dac21a169368d0
tsubakimoto_s <tkmbmbr@gmail.com>
2014-06-15 21:57:27 +0900

    #06-#09

--- basic_javascript_v2/myscript.ts
+++ basic_javascript_v2/myscript.ts
@@ -6,3 +6,31 @@
 //var s = "5" + 5;
 //alert(s);
 
+//var score = 60;
+//if (score == 60) {
+//    alert("ok");
+//} else {
+//    alert("ng");
+//}
+
+//var signal = "yellow";
+//switch (signal) {
+//    case "red":
+//        alert("stop!");
+//        break;
+//    case "green":
+//        alert("go!");
+//        break;
+//    case "yellow":
+//        alert("slow down!");
+//        break;
+//    default:
+//        alert("wrong signal");
+//        break;
+//}
+
+var i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
